67da3c930ceefbffab3c781aa09ab7c09ce8a67e
Yoadad <yoadad.santibanez@devfacto.com>
2015-01-11 18:06:30 -0600

    Fixing html angular js and css and adding favicon.ico

--- FileManipulation/base/js/base.js
+++ FileManipulation/base/js/base.js
@@ -1,13 +1,14 @@
-var baseApp = angular.module('baseApp',[]);
+var {*}App = angular.module('{*}App',[]);
 
-var baseCtrl = baseApp.controller('baseCtrl',function($scope){
+var {*}Ctrl = {*}App.controller('{*}Ctrl',function($scope){
 
     $scope.items = [
 		{'value':13,'name':'Soda',img:'img/soda.png'}
 		,{'value':55,'name':'Hamburger',img:'img/hamburger.png'}
+		,{'value':8,'name':'French Fries',img:'img/frenchFries.png'}
 		,{'value':13,'name':'Coffe',img:'img/coffe.png'}
 		,{'value':21,'name':'Muffin',img:'img/muffin.png'}
-		,{'value':8,'name':'French Fries',img:'img/frenchFries.png'}
+		
 	];
 
 });
--- FileManipulation/htmlAngularBase.ps1
+++ FileManipulation/htmlAngularBase.ps1
@@ -1,6 +1,6 @@
 ﻿clear
 
-$mainFolderName = 'base_'
+$mainFolderName = 'test1'
 
 $configPath = "$PSSCRIPTROOT\htmlAngularBAse.json"
 $config = (Get-Content $configPath) -join "`n" | ConvertFrom-Json
@@ -8,11 +8,16 @@ $config = (Get-Content $configPath) -join "`n" | ConvertFrom-Json
 $baseFolder = "$PSSCRIPTROOT\base"
 $mainFolder = $config.path + '\' + $mainFolderName
 
+
+Remove-Item $mainFolder -Force -Recurse
 New-Item -ItemType Directory -Force -Path $mainFolder
 
 Copy-Item -Path "$baseFolder\*" -Destination $mainFolder –Recurse
 
-New-Item "$mainFolder\js\$mainFolderName.js" -type file -Force
-New-Item "$mainFolder\css\$mainFolderName.css" -type file -Force
 
 (gc "$mainFolder\index.html").replace('{*}',$mainFolderName)|sc "$mainFolder\index.html"
+(gc "$mainFolder\js\base.js").replace('{*}',$mainFolderName)|sc "$mainFolder\js\base.js"
+
+Rename-Item "$mainFolder\js\base.js" "$mainFolderName.js"
+Rename-Item "$mainFolder\css\base.css" "$mainFolderName.css"
+
