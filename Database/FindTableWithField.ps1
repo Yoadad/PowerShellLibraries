df1d6f2f3c96c2ca4e45963152696050a883c080
Edson Alcalá <edson.alcala@devfacto.com>
2015-03-31 11:09:34 -0400

    added tools for resize a set of images in a folder and also a converter script

--- Database/FindTableWithField.ps1
+++ Database/FindTableWithField.ps1
@@ -36,3 +36,64 @@ Function FindTableWithField($paramField)
     $table
     $connection.Close()
 }
+
+
+Function GetConnectionString()
+{
+    $json = [System.IO.File]::ReadAllText("$PSScriptRoot\config.json")
+    $config = ConvertFrom-Json $json
+    return $config.connectionString
+}
+
+Function Getconnection()
+{
+    $connectionString = GetConnectionString
+    $connection = New-Object System.Data.SqlClient.SqlConnection
+    $connection.ConnectionString = $connectionString
+    return $connection
+}
+
+Function GetDataFromQuery($query)
+{
+    $connection = Getconnection
+    $connection.Open()
+    $command = $connection.CreateCommand()
+    $command.CommandText = $query
+    $result = $command.ExecuteReader()
+    $table = new-object “System.Data.DataTable”
+    $table.Load($result)
+    $connection.Close()
+    return $table
+}
+
+Function ExcecuteQuery($query)
+{
+    $connection = Getconnection
+    $connection.Open()
+    $command = $connection.CreateCommand()
+    $command.CommandText = $query
+    $command.ExecuteNonQuery()
+    $connection.Close()
+}
+
+
+Function GetDBHasTable($tableName)
+{
+    $table = GetDataFromQuery “SELECT * FROM sys.databases"
+
+    foreach($row in $table){
+        $dbName = $row[0]
+        if($dbName -ne "master"){
+            $query = “USE " + $dbName + ";SELECT name FROM sys.tables WHERE name like '%"+$tableName+"%';"
+            $result = GetDataFromQuery $query
+            
+            if($result -ne $null){
+                $db = [System.String]::Format("{0}",$dbName)
+                echo $db
+                echo "**********************"
+                echo $result
+            }
+        }
+    }
+    
+}
---
+++ Tools/Convert-LogToObject.ps1
@@ -0,0 +1,16 @@
+﻿$documentInfo = Get-Content "C:\Users\Panzergrenadier\Desktop\DotNet\916\LogToAnalyze3.txt"
+$arrayItem = [System.Collections.ArrayList]@()
+
+#InvokerMethodName
+#InvokerClassNam
+#StoreProcedureName
+#Parameters
+foreach($item in $documentInfo){
+   $index = $item.IndexOf("Invoker")
+   $lenghtOfItem = $item.Length
+   $itemSubstracted = $item.Substring($index - 3)
+   $jsonTest = ConvertFrom-Json $itemSubstracted   
+   $arrayItem.Add($jsonTest)
+}
+$arrayItem | select InvokerMethodName,StoreProcedureName, InvokerClassName | Sort-Object -Property InvokerMethodName -Unique
+
