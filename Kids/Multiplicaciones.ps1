9732daeeff0adc7be27de44cfe3c3d99ca5805a4
yoadad <yoadad@gmail.com>
2016-07-28 17:46:20 -0500

    adding game

---
+++ Kids/Multiplicaciones.ps1
@@ -0,0 +1,30 @@
+﻿clear
+$score = 0
+while($true){
+    
+    $sm = [System.String]::Format("Score: {0}", $score)
+    echo $sm
+
+    $x = Get-Random -Maximum 12 -Minimum 1
+    $y = Get-Random -Maximum 12 -Minimum 1
+    $m = [System.String]::Format("{0} x {1} = ?",$x,$y)
+    $r = $x*$y
+
+    echo $m
+    #echo $r
+
+    $ur = Read-Host 'Respuesta'
+    
+    clear
+
+    if ($ur -eq $r){
+        echo '¡¡Muy bien!! :)'
+        $score++
+    }
+    else{
+        echo '¡¡Necesitas estudiar mas!! :('
+        $score--
+    }
+
+   
+}
